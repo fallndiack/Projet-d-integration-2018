00296fdfed7c2238b1e22ae364717c161aed72b7
fallndiack <fallndiack1@gmail.com>
2019-03-19 19:58:18 +0100

    combats a venirpour user

---
+++ Lamb_Ji_UI/Controllers/AfficheUserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lamb_Ji_DAL;
+
+namespace Lamb_Ji_UI.Controllers
+{
+    public class AfficheUserController : Controller
+    {
+        private CNGLUTTEDBEntities db = new CNGLUTTEDBEntities();
+
+        // GET: AfficheUser
+        public ActionResult Index()
+        {
+            var affiches = db.Affiches.Include(a => a.AvisAffiche).Include(a => a.Lutteur).Include(a => a.Lutteur1).Include(a => a.Combat);
+            return View(affiches.ToList());
+        }
+
+        // GET: AfficheUser/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Affiche affiche = db.Affiches.Find(id);
+            if (affiche == null)
+            {
+                return HttpNotFound();
+            }
+            return View(affiche);
+        }
+
+      
+    }
+}
--- Lamb_Ji_UI/Controllers/MenuAccueilController.cs
+++ Lamb_Ji_UI/Controllers/MenuAccueilController.cs
@@ -20,9 +20,9 @@ namespace Lamb_Ji_UI.Controllers
         {
 
             List<MenuDynamique> list = new List<MenuDynamique>();
-            list.Add(new MenuDynamique { Lien = "#", NomLien = "ACCUEIL" });
+            list.Add(new MenuDynamique { Lien = "Home/Index", NomLien = "ACCUEIL" });
             list.Add(new MenuDynamique { Lien = "/Lutteur/DisplayLuteurForUser", NomLien = "LUTTEURS" });
-            list.Add(new MenuDynamique { Lien = "#", NomLien = "COMBATS A VENIR" });
+            list.Add(new MenuDynamique { Lien = "AfficheUser/Index", NomLien = "COMBATS A VENIR" });
             list.Add(new MenuDynamique { Lien = "#", NomLien = "ACTUALITES" });
             list.Add(new MenuDynamique { Lien = "/Gallerie/Index", NomLien = "Gallerie de Lutte" });
             list.Add(new MenuDynamique { Lien = "/TypeLutte/Index", NomLien = "Les Types de Lutte" });
